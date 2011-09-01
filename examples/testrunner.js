95415b24c745bd5e73062b2d2c223520442f0438
Luke Schafer <luke.schafer@gmail.com>
2011-09-02 08:39:42 +1000

    working on changing ti add(fxture) then run()

--- examples/testrunner.js
+++ examples/testrunner.js
@@ -1,2 +1,2 @@
 #!/usr/bin/env node
-var noderunner = require('../index.js').setup({showSuccesses:true, suppressConsole:true}).runFromDir(require('path').resolve('./tests'));
+var noderunner = require('../index.js').setup({showSuccesses:true, suppressConsole:true}).addDirectory(require('path').resolve('./tests')).run();
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var fs = require( 'fs' );
 var path = require('path');
+var assert = require('assert');
 
 //internal
 var successes = 0, failures = 0, ignored = 0, totalFixtures = 0, fixturesRun = 0, completed = false;
+var fixtures = {};
 
 //options
 var showSuccesses = false, suppressConsole = false, useColors = true, writeToConsole = true;
@@ -29,29 +31,50 @@ noderunner.setup = function(config) {
 		console.error = function(){};
 	}
 	return noderunner;
+};
+
+noderunner.reset = function() {
+	successes = 0;
+	failures = 0;
+	ignored = 0;
+	totalFixtures = 0;
+	fixturesRun = 0;
+	completed = false;
+	fixtures = {};
+	return noderunner;
 }
 
-noderunner.runFromDir = function(p) {
-	var fixtures = {};
+noderunner.addDirectory = function(p) {
 	fs.readdir(p, function( err, files ) {
 		files.forEach(function(file) {
 			var jsExt = /[.]js$/i;
 			if (!file.match(jsExt)) return;
 			var fixtureName = file.replace(jsExt,'');
 			printlog('Loading tests: ' + fixtureName + " in " + path.join(p, file));
-			fixtures[fixtureName] = require(path.join(p, file));
+			var t = require(path.join(p, file));
+			noderunner.add(fixtureName, t);
 		});
-		
-		runAll(fixtures);
 	});
 	return noderunner;
 }
 
-var runAll = noderunner.runAll = function(fixtures) {
+noderunner.add = function (fixtureName, tests) {
+	assert.ok('string' == typeof fixtureName, 'fixtureName should be a string');
+	assert.ok('object' == typeof tests, 'tests must be an object');
+	fixtures[fixtureName] = tests;
+	noderunner.emit('addfixture', fixtureName, tests);
+}
+
+noderunner.addAll = function (fixs) {
+	assert.ok('object' == typeof fixs, 'fixtures to add must be an object {key:tests}');
+	for (var k in fixs) if (fixs.hasOwnProperty(k)) noderunner.add(k, fixs[k]);
+}
+
+var run = noderunner.run = function() {
 	var fixtureNames = [], i = 0;
 	for (var k in fixtures) { fixtureNames.push(k); }
 	totalFixtures = fixtureNames.length;
-	
+	printlog(JSON.stringify(fixtures));
 	(function proceed() {
 		if (i >= fixtureNames.length) {
 			complete();
@@ -60,7 +83,7 @@ var runAll = noderunner.runAll = function(fixtures) {
 		var k = fixtureNames[i];
 		++i;
 		noderunner.emit('running', k);
-		run(k, fixtures[k], function() {
+		runOne(k, fixtures[k], function() {
 			++fixturesRun;
 			proceed();
 		}, false);
@@ -68,69 +91,65 @@ var runAll = noderunner.runAll = function(fixtures) {
 	return noderunner;
 }
 
-var run = noderunner.run = function (fixtureName, tests, callback, reportWhenDone) {
-	// requeue so chained calls have the ability to subscribe to events before we run tests
-	process.nextTick(function() {
-		// remove cached modules so it's clean!
-		var m = require('module');
-		for (var k in m._cache) {
-			if (k.indexOf('node_modules'))
-			delete m._cache[k];
-		}
-		
-		function placeholder(done) { done(); }
-		var fixtureSetup = tests.fixtureSetup || placeholder;
-		var fixtureTeardown = tests.fixtureTeardown || placeholder;
-		var setup = tests.setup || placeholder;
-		var teardown = tests.teardown || placeholder;
-		
-		var testNames = [], i = 0;
-		for (var k in tests) { testNames.push(k); }
-		
-		fixtureSetup(function() {
-			(function proceed(k) {
-				if (i >= testNames.length) return fixtureTeardown(callback);
-				var k = testNames[i];
-				++i;
-				
-				if (k == 'fixtureSetup' || k == 'fixtureTeardown' || k == 'setup' || k == 'teardown') return proceed();
-				
-				setup(function() {
-					var isAsync = false;
-					function async(callback) {
-						isAsync = true;
-						async.isComplete = false;
-						async.myCallback = callback;
-						noderunner.once('beforeExit', async.complete);
-					};
-					async.complete = function complete(){
-						if (async.isComplete) return; async.isComplete = true;
-						try {
-							async.myCallback();
-							noderunner.emit('success', fixtureName, k);
-						} catch (e) {
-							noderunner.emit('failure', fixtureName, k, e);
-						}
-						teardown(function() { proceed(); });
-					}
-					
+function runOne (fixtureName, tests, callback, reportWhenDone) {
+	// remove cached modules so it's clean!
+	var m = require('module');
+	for (var k in m._cache) {
+		if (k.indexOf('node_modules'))
+		delete m._cache[k];
+	}
+	
+	function placeholder(done) { done(); }
+	var fixtureSetup = tests.fixtureSetup || placeholder;
+	var fixtureTeardown = tests.fixtureTeardown || placeholder;
+	var setup = tests.setup || placeholder;
+	var teardown = tests.teardown || placeholder;
+	
+	var testNames = [], i = 0;
+	for (var k in tests) { testNames.push(k); }
+	
+	fixtureSetup(function() {
+		(function proceed(k) {
+			if (i >= testNames.length) return fixtureTeardown(callback);
+			var k = testNames[i];
+			++i;
+			
+			if (k == 'fixtureSetup' || k == 'fixtureTeardown' || k == 'setup' || k == 'teardown') return proceed();
+			
+			setup(function() {
+				var isAsync = false;
+				function async(callback) {
+					isAsync = true;
+					async.isComplete = false;
+					async.myCallback = callback;
+					noderunner.once('beforeExit', async.complete);
+				};
+				async.complete = function complete(){
+					if (async.isComplete) return; async.isComplete = true;
 					try {
-						var resp = tests[k](async);
-						if (resp && resp['ignore']) {
-							isAsync = false;
-							noderunner.emit('ignore', fixtureName, k, resp['ignore'])
-						} else {
-							if (!isAsync) noderunner.emit('success', fixtureName, k);
-						}
+						async.myCallback();
+						noderunner.emit('success', fixtureName, k);
 					} catch (e) {
 						noderunner.emit('failure', fixtureName, k, e);
 					}
-					if (!isAsync) teardown(function() { teardown(proceed); });
-				});
-			})(k);
-		});
+					teardown(function() { proceed(); });
+				}
+				
+				try {
+					var resp = tests[k](async);
+					if (resp && resp['ignore']) {
+						isAsync = false;
+						noderunner.emit('ignore', fixtureName, k, resp['ignore'])
+					} else {
+						if (!isAsync) noderunner.emit('success', fixtureName, k);
+					}
+				} catch (e) {
+					noderunner.emit('failure', fixtureName, k, e);
+				}
+				if (!isAsync) teardown(function() { teardown(proceed); });
+			});
+		})(k);
 	});
-	return noderunner;
 }
 
 function complete() {
@@ -172,6 +191,11 @@ process.once('exit', function() {
 noderunner.on('running', function(test) {
 	if (writeToConsole)
 		printlog('\nRunning ' + test);
+});
+
+noderunner.on('addfixture', function(name, tests) {
+	if (writeToConsole)
+		printlog('added fixture ' + name);
 });
 
 noderunner.on('failure', function(fixtureName, name, ex) {
